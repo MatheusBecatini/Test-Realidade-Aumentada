04b24bc3ef652c354d2f5a0a3f734b655035a7b5
Matheus Becatini <60360393+matheus-becatini@users.noreply.github.com>
2020-04-30 15:35:25 -0300

    Link

--- js/LocationDynamic.js
+++ js/LocationDynamic.js
@@ -49,6 +49,7 @@ window.onload = () => {
                     placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     placeText.setAttribute('title', place.name);
                     placeText.setAttribute('scale', '15 15 15');
+                    placeText.setAttribute('href', 'http://pudim.com.br/');
                     
                     placeText.addEventListener('loaded', () => {
                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
