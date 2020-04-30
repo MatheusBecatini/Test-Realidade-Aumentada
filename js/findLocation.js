c16a7d6e54e7ea5e97f945315140d393683d6a83
Matheus Becatini <60360393+matheus-becatini@users.noreply.github.com>
2020-04-30 12:52:42 -0300

    location Script

--- js/findLocation.js
+++ js/findLocation.js
@@ -1,7 +1,5 @@
-const FindLocation = () => {
-    window.onload = () => {navigator.geolocation.getCurrentPosition((position) => {
-        document.querySelector('a-gltf-model')
-            .setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude};`)
-      });
-     }
-} 
+window.onload = () => {navigator.geolocation.getCurrentPosition((position) => {
+    document.querySelector('a-gltf-model')
+        .setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude};`)
+    });
+}
