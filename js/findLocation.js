84452a2ab41acce254386bb2fcfbb8a630e2d64d
Matheus Becatini <60360393+matheus-becatini@users.noreply.github.com>
2020-05-05 15:28:50 -0300

    Orientado a APP

--- js/findLocation.js
+++
@@ -1,5 +0,0 @@
-window.onload = () => {navigator.geolocation.getCurrentPosition((position) => {
-    document.querySelector('a-gltf-model')
-        .setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude};`)
-    });
-}
